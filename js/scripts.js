148d6920ffdcce514470ebbc46484ac4886aa2c4
matt caswell and Shruti Priya <matt@>
2017-01-25 14:15:28 -0800

    add code to the scripts file to get the basic functionality done for the player1

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,15 @@ var Player = function(playerName){
   this.turnScore = 0;
   this.gameScore = 0;
 }
-
+ // Pick a random number between 1 and 6
 var rollDice = function() {
   return (Math.floor((Math.random() * 6) + 1));
 }
+
+Player.prototype.rollHold = function(){
+  this.gameScore += this.turnScore;
+}
+
 
 // User logic goes here....
 $(document).ready(function(){
@@ -18,10 +23,22 @@ $(document).ready(function(){
 
     var thisRoll = rollDice();
     player.turnScore += thisRoll;
-    if (thisRoll ===0){
+    if (thisRoll ===1){
      player.turnScore = 0;
       }
-    $(".turnscore1").text(player.turnScore);
-    $("#result1").text();
-  })
+      $(".turnscore1").text(player.turnScore);
+      $(".rollscore").text(thisRoll);
+
+    })
+
+    $(".hold").click(function(){
+      event.preventDefault();
+     player.rollHold();
+     $(".turnscore1").text(player.turnScore);
+     $(".rollscore").text("");
+     console.log(player.gameScore);
+     $(".gamescore1").text(player.gameScore);
+
+    })
 })
+// $("#result1").text();
